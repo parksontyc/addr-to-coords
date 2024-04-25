aea687dbefb1cb99e745236dc31fc668b6eda41b
parksontyc <parkson.tyc.chen@gmail.com>
2024-04-25 14:17:34 +0800

    add configure file

---
+++ addr_to_coords/configure.py
@@ -0,0 +1,19 @@
+import os
+
+from dotenv import load_dotenv
+import requests
+
+
+url = 'https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token'
+headers = {'content-type': 'application/x-www-form-urlencoded'}
+load_dotenv()
+
+data = {
+    'grant_type': 'client_credentials',
+    'client_id': f'{os.getenv("client_id")}',
+    'client_secret': f'{os.getenv("client_secret")}'
+}
+
+response = requests.post(url, headers=headers, data=data)
+print(response.json())
+token = response.json()['access_token']
